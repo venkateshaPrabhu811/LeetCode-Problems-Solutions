be7b5b5785afe2b42aa906f2823ece115ac25f8b
venkateshaPrabhu811 <101506797+venkateshaPrabhu811@users.noreply.github.com>
2022-03-15 19:48:13 +0530

    Time: 24 ms (72.32%), Space: 51.4 MB (53.42%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
@@ -1,6 +1,6 @@
 class Solution {
     public String minRemoveToMakeValid(String s) {
-        Stack<Integer> stk = new Stack<>();
+        /*Stack<Integer> stk = new Stack<>();
         char[] arr = s.toCharArray();
         for(int i = 0;i<s.length();i++){
             if(arr[i] == '('){
@@ -18,6 +18,43 @@ class Solution {
         while(!stk.isEmpty()){
             arr[stk.peek()] = '#';
             stk.pop();
+        }
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0;i<s.length();i++){
+            if(arr[i] != '#'){
+                sb.append(arr[i]);
+            }
+        }
+        return sb.toString();*/
+        
+        char[] arr = s.toCharArray();
+        int count = 0;
+        for(int i = 0;i<s.length();++i){
+            if(arr[i] == '('){
+                ++count;
+            }
+            else if(arr[i] == ')'){
+                if(count == 0){
+                    arr[i] = '#';
+                }
+                else{
+                    count--;
+                }
+            }
+        }
+        count = 0;
+        for(int i = s.length()-1;i>=0;--i){
+            if(arr[i] == ')'){
+                count++;
+            }
+            else if(arr[i] == '('){
+                if(count == 0){
+                    arr[i] = '#';
+                }
+                else{
+                    count--;
+                }
+            }
         }
         StringBuilder sb = new StringBuilder();
         for(int i = 0;i<s.length();i++){
