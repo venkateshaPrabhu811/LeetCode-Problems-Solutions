f0c77d55d1fcfb507d828647af911296e7876601
venkateshaPrabhu811 <101506797+venkateshaPrabhu811@users.noreply.github.com>
2022-03-17 18:51:18 +0530

    Time: 1 ms (68.54%), Space: 41.7 MB (55.10%) - LeetHub

--- 856-score-of-parentheses/856-score-of-parentheses.java
+++ 856-score-of-parentheses/856-score-of-parentheses.java
@@ -1,6 +1,6 @@
 class Solution {
     public int scoreOfParentheses(String s) {
-        Stack<Integer> stk = new Stack<>();
+        /*Stack<Integer> stk = new Stack<>();
         int len = s.length();
         int score = 0;
         for(int i = 0;i<len;i++){
@@ -13,6 +13,33 @@ class Solution {
                 score = stk.pop() + Math.max(2*score,1);
             }
         }
-        return score;
+        return score;*/
+        
+        Stack<Integer> stk = new Stack<>();
+        for(int i = 0;i<s.length();i++){
+            char ch = s.charAt(i);
+            if(ch == '('){
+                stk.push(0);
+            }
+            else{
+                if(stk.peek() == 0){
+                    stk.pop();
+                    stk.push(1);
+                }
+                else{
+                    int ans = 0;
+                    while(stk.peek() != 0){
+                        ans += stk.pop();
+                    }
+                    stk.pop();
+                    stk.push(ans*2);
+                }
+            }
+        }
+        int ans = 0;
+        while(!stk.isEmpty()){
+            ans += stk.pop();
+        }
+        return ans;
     }
 }
