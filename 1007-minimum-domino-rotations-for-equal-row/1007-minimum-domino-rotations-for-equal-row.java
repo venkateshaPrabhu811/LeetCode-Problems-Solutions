5e8f7cd2a8d5ca6ede3d04a3f4c666c6bdf01732
venkateshaPrabhu811 <101506797+venkateshaPrabhu811@users.noreply.github.com>
2022-03-20 12:23:26 +0530

    Time: 10 ms (24.55%), Space: 94.6 MB (27.70%) - LeetHub

--- 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.java
+++ 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.java
@@ -1,6 +1,7 @@
 class Solution {
     public int minDominoRotations(int[] tops, int[] bottoms) {
-        int[] count1 = new int[7];
+        //Brute Force
+        /*int[] count1 = new int[7];
         int[] count2 = new int[7];
         int max = 0;
         int dir = 0;
@@ -47,6 +48,24 @@ class Solution {
                 }
             }
         }
-        return swaps;
+        return swaps;*/
+        
+        int[] topCount = new int[7];
+        int[] bottomCount = new int[7];
+        int[] sameCount = new int[7];
+        for(int i = 0;i<tops.length;i++){
+            ++topCount[tops[i]];
+            ++bottomCount[bottoms[i]];
+            if(tops[i] == bottoms[i]){
+                ++sameCount[tops[i]];
+            }
+        }
+        int minRotations = Integer.MAX_VALUE;
+        for(int i = 1;i<7;i++){
+            if(topCount[i] + bottomCount[i] - sameCount[i] == tops.length){
+                minRotations = Math.min(minRotations,Math.min(topCount[i],bottomCount[i]) - sameCount[i]);
+            }
+        }
+        return minRotations == Integer.MAX_VALUE ? -1 : minRotations;
     }
 }
