a3f3816130a4ffcfa5b4ce0c8c0bff506b3e8ceb
venkateshaPrabhu811 <101506797+venkateshaPrabhu811@users.noreply.github.com>
2022-04-09 18:50:28 +0530

    Time: 31 ms (8.65%), Space: 50.9 MB (29.21%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.java
@@ -4,31 +4,18 @@ class Solution {
         for(int i = 0;i<nums.length;i++){
             freq.put(nums[i],freq.getOrDefault(nums[i],0)+1);
         }
-        PriorityQueue<count> pq = new PriorityQueue<>(new CustComp());
-        for(Map.Entry<Integer,Integer> entry : freq.entrySet()){
-            count ob = new count(entry.getKey(),entry.getValue());
-            pq.offer(ob);
+        PriorityQueue<Integer> pq = new PriorityQueue<>(
+            (n1,n2) -> freq.get(n1) - freq.get(n2));
+        for(int key : freq.keySet()){
+            pq.offer(key);
+            if(pq.size() > k){
+                pq.poll();
+            }
         }
-        List<Integer> res = new ArrayList<>();
-        while(pq.size() > k){
-            pq.poll();
+        int[] res = new int[k];
+        for(int i = 0;i<k;i++){
+            res[i] = pq.poll();
         }
-        while(pq.size() > 0){
-            res.add(pq.poll().num);
-        }
-        return res.stream().mapToInt(i -> i).toArray();
-    }
-}
-class CustComp implements Comparator<count>{
-    public int compare(count a,count b){
-        return a.fr - b.fr;
-    }
-}
-class count{
-    int num;
-    int fr;
-    count(int a,int b){
-        this.num = a;
-        this.fr = b;
+        return res;
     }
 }
