0b41037beecfe19225311fead0ca3d275f1f59d9
venkateshaPrabhu811 <101506797+venkateshaPrabhu811@users.noreply.github.com>
2022-04-22 18:49:17 +0530

    Time: 238 ms (8.15%), Space: 60.1 MB (72.59%) - LeetHub

--- 706-design-hashmap/706-design-hashmap.java
+++ 706-design-hashmap/706-design-hashmap.java
@@ -1,20 +1,53 @@
 class MyHashMap {
-    int[] hm;
+    class Node{
+        int key;
+        int val;
+        Node next;
+        Node(int key,int val){
+            this.key = key;
+            this.val = val;
+            this.next = null;
+        }
+    }
+    Node root,counter;
     public MyHashMap() {
-        hm = new int[1_000_001];
-        Arrays.fill(hm,-1);
+        root = new Node(-1,-1);
+        counter = root;
     }
     
     public void put(int key, int value) {
-        hm[key] = value;
+        Node temp = root.next;
+        while(temp != null){
+            if(temp.key == key){
+                temp.val = value;
+                return;
+            }
+            temp = temp.next;
+        }
+        counter.next = new Node(key,value);
+        counter = counter.next;
     }
     
     public int get(int key) {
-        return hm[key];
+        Node temp = root.next;
+        while(temp != null){
+            if(temp.key == key){
+                return temp.val;
+            }
+            temp = temp.next;
+        }
+        return -1;
     }
     
     public void remove(int key) {
-        hm[key] = -1;
+        Node temp = root.next;
+        while(temp != null){
+            if(temp.key == key){
+                temp.val = -1;
+                return;
+            }
+            temp = temp.next;
+        }
     }
 }
 
