050ba0d736e7390eb9e94c32674304ee97b12430
venkateshaPrabhu811 <101506797+venkateshaPrabhu811@users.noreply.github.com>
2022-04-23 19:11:11 +0530

    Time: 13 ms (14.99%), Space: 46.3 MB (5.19%) - LeetHub

--- 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.java
+++ 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.java
@@ -2,12 +2,22 @@ public class Codec {
 
     // Encodes a URL to a shortened URL.
     public String encode(String longUrl) {
-        return longUrl;
+        StringBuilder url = new StringBuilder(longUrl);
+        for(int i = 0;i<url.length();i++){
+            char ch = (char)(url.charAt(i) ^ url.length());
+            url.replace(i,i+1,Character.toString(ch));
+        }
+        return url.toString();
     }
 
     // Decodes a shortened URL to its original URL.
     public String decode(String shortUrl) {
-        return shortUrl;
+        StringBuilder url = new StringBuilder(shortUrl);
+        for(int i = 0;i<url.length();i++){
+            char ch = (char)(url.charAt(i) ^ url.length());
+            url.replace(i,i+1,Character.toString(ch));
+        }
+        return url.toString();
     }
 }
 
