d844273270bbd4b71d286a5a42fd10239ee671a1
venkateshaPrabhu811 <101506797+venkateshaPrabhu811@users.noreply.github.com>
2022-05-01 10:19:28 +0530

    Time: 1 ms (87.56%), Space: 42.8 MB (13.61%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.java
+++ 844-backspace-string-compare/844-backspace-string-compare.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean backspaceCompare(String s, String t) {
-        Stack<Character> stk1 = new Stack<>();
+        /*Stack<Character> stk1 = new Stack<>();
         Stack<Character> stk2 = new Stack<>();
         int ptr1 = 0;
         int ptr2 = 0;
@@ -27,6 +27,46 @@ class Solution {
             if(stk1.pop() != stk2.pop()){
                 return false;
             }
+        }
+        return true;*/
+        int left = s.length()-1;
+        int right = t.length()-1;
+        int spaceLeft = 0;
+        int spaceRight = 0;
+        while(left >= 0 || right >= 0){
+            while(left >= 0){
+                char ch = s.charAt(left);
+                if(ch == '#'){
+                    spaceLeft++;
+                    left--;
+                }
+                else if(spaceLeft > 0){
+                    left--;
+                    spaceLeft--;
+                }
+                else{
+                    break;
+                }
+            }
+            while(right >= 0){
+                char ch = t.charAt(right);
+                if(ch == '#'){
+                    spaceRight++;
+                    right--;
+                }
+                else if(spaceRight > 0){
+                    right--;
+                    spaceRight--;
+                }
+                else{
+                    break;
+                }
+            }
+            if(left < 0 && right < 0) return true;
+            if(left >= 0 && right >= 0 && s.charAt(left) != t.charAt(right)) return false;
+            if(left < 0 || right < 0) return false;
+            left--;
+            right--;
         }
         return true;
     }
