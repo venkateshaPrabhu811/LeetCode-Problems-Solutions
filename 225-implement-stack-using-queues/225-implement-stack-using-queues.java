22e82f0d4f045b91a9a775eb9ac2dc31b293bec0
venkateshaPrabhu811 <101506797+venkateshaPrabhu811@users.noreply.github.com>
2022-05-05 17:58:46 +0530

    Time: 0 ms (100.00%), Space: 42.2 MB (20.76%) - LeetHub

--- 225-implement-stack-using-queues/225-implement-stack-using-queues.java
+++ 225-implement-stack-using-queues/225-implement-stack-using-queues.java
@@ -1,19 +1,22 @@
 class MyStack {
-    Deque<Integer> q1;
+    Queue<Integer> q1;
     public MyStack() {
-        q1 = new ArrayDeque<>();
+        q1 = new LinkedList<>();
     }
     
     public void push(int x) {
-        q1.offer(x);
+        q1.add(x);
+        for(int i = 1;i<q1.size();i++){
+            q1.add(q1.remove());
+        }
     }
     
     public int pop() {
-        return q1.removeLast();
+        return q1.remove();
     }
     
     public int top() {
-        return q1.getLast();
+        return q1.peek();
     }
     
     public boolean empty() {
