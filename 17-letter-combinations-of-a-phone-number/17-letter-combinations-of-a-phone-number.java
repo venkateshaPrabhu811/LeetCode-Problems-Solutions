68372f44b485b55582656d81d0b40ee10c7208b5
venkateshaPrabhu811 <101506797+venkateshaPrabhu811@users.noreply.github.com>
2022-05-09 12:05:17 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (85.46%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
@@ -1,8 +1,9 @@
 class Solution {
-    List<String> ans = new ArrayList<>();
-    HashMap<Character,String> map = new HashMap<>();
+    //Backtracking Approach
+    //List<String> ans = new ArrayList<>();
+    //HashMap<Character,String> map = new HashMap<>();
     public List<String> letterCombinations(String digits) {
-        if(digits.length() == 0){
+        /*if(digits.length() == 0){
             return ans;
         }
         map.put('2',"abc");
@@ -14,10 +15,32 @@ class Solution {
         map.put('8',"tuv");
         map.put('9',"wxyz");
         helper(0,new String(),digits);
+        return ans;*/
+        
+        
+        //To reduce Time Complexity
+        List<String> ans = new ArrayList<>();
+        if(digits.length() == 0) return ans;
+        String[] letters = {"abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+        helper(0,ans,letters,new StringBuilder(),digits);
         return ans;
+    }
+    //Slightly Optimized
+    private void helper(int curr,List<String> ans,String[] letters,StringBuilder sb,String digits){
+        if(curr == digits.length()){
+            ans.add(sb.toString());
+            return;
+        }
+        String str = letters[digits.charAt(curr) - '0' - 2];
+        for(int i = 0;i<str.length();i++){
+            sb.append(str.charAt(i));
+            helper(curr+1,ans,letters,sb,digits);
+            sb.deleteCharAt(sb.length()-1);
+        }
         
     }
-    private void helper(int curr,String res,String digits){
+    
+    /*private void helper(int curr,String res,String digits){
         if(curr == digits.length()) return;
         String str = map.get(digits.charAt(curr));
         for(int i = 0;i<str.length();i++){
@@ -27,5 +50,5 @@ class Solution {
                 ans.add(res);
             res = res.substring(0,res.length()-1);
         }
-    }
+    }*/
 }
