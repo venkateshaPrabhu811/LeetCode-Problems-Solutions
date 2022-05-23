66fecd35eb8c37b0601d520b6b2f47fec7419530
venkateshaPrabhu811 <101506797+venkateshaPrabhu811@users.noreply.github.com>
2022-05-23 22:00:20 +0530

    Time: 37 ms (72.12%), Space: 42.6 MB (74.98%) - LeetHub

--- 474-ones-and-zeroes/474-ones-and-zeroes.java
+++ 474-ones-and-zeroes/474-ones-and-zeroes.java
@@ -1,12 +1,22 @@
 class Solution {
-    int[][][] dp;//recursive along with dp
+    int[][] dp;
     public int findMaxForm(String[] strs, int m, int n) {
-        //Recursive Approach
-        dp = new int[m+1][n+1][strs.length];
-        return helper(strs,m,n,0);
+        dp = new int[m+1][n+1];
+        //dp bottom up approach
+        for(String s : strs){
+            int[] count = count(s);
+            for(int zero = m;zero >= count[0];zero--){
+                for(int one = n;one >= count[1];one--){
+                    dp[zero][one] = Math.max(dp[zero - count[0]][one-count[1]] + 1,dp[zero][one]);
+                }
+            }
+        }
+        return dp[m][n];
+        //return helper(strs,m,n,0);
         
     }
-    private int helper(String[] str,int m,int n,int index){
+    //Recursive Approach with dp
+    /*private int helper(String[] str,int m,int n,int index){
         if(index == str.length || (m == 0 && n == 0)) return 0;
         
         if(dp[m][n][index] > 0) return dp[m][n][index];
@@ -18,7 +28,7 @@ class Solution {
         
         dp[m][n][index] = Math.max(consider,skip);
         return dp[m][n][index];
-    }
+    }*/
     private int[] count(String str){
         int[] count = new int[2];
         for(char ch : str.toCharArray()){
