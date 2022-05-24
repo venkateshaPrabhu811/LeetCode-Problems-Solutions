3aab140bd37e5c6ebe8e8c95e78b93caea3baa3c
venkateshaPrabhu811 <101506797+venkateshaPrabhu811@users.noreply.github.com>
2022-05-24 18:10:56 +0530

    Time: 2 ms (90.88%), Space: 42.4 MB (72.17%) - LeetHub

--- 32-longest-valid-parentheses/32-longest-valid-parentheses.java
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.java
@@ -1,6 +1,7 @@
 class Solution {
     public int longestValidParentheses(String s) {
-        Stack<Integer> stk = new Stack<>();
+        //Normal stack approach
+        /*Stack<Integer> stk = new Stack<>();
         stk.add(-1);
         int max = 0;
         for(int i = 0;i<s.length();i++){
@@ -18,6 +19,36 @@ class Solution {
                     max = Math.max(len,max);
                 }
             }
+        }
+        return max;*/
+        int open = 0;
+        int close = 0;
+        int max = 0;
+        char ch;
+        for(int i = 0;i<s.length();i++){
+            ch = s.charAt(i);
+            if(ch == '(')open++;
+            else close++;
+            
+            if(open == close){
+                int len = open + close;
+                max = Math.max(len,max);
+            }
+            else if(close > open)
+                open = close = 0;
+        }
+        open = close = 0;
+        for(int i = s.length()-1;i>=0;i--){
+            ch = s.charAt(i);
+            if(ch == '(') open++;
+            else close++;
+            
+            if(open == close){
+                int len = open + close;
+                max = Math.max(len,max);
+            }
+            else if(open > close)
+                open = close = 0;
         }
         return max;
     }
