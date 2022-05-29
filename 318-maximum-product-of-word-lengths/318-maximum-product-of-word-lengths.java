5c4dd5deafa71037a11ad91e871bc5d17cfeb5dc
venkateshaPrabhu811 <101506797+venkateshaPrabhu811@users.noreply.github.com>
2022-05-29 19:44:21 +0530

    Time: 15 ms (59.30%), Space: 47.8 MB (41.62%) - LeetHub

--- 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.java
+++ 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.java
@@ -1,7 +1,7 @@
 class Solution {
     public int maxProduct(String[] words) {
         //brute force with every character array
-        int[][] dp = new int[words.length][26];//for counting evey character in every word
+        /*int[][] dp = new int[words.length][26];//for counting evey character in every word
         int max = 0;
         for(int i = 0;i<words.length;i++){
             for(int j = 0;j<words[i].length();j++){
@@ -22,7 +22,25 @@ class Solution {
             if(dp[i][k] == 1 && dp[j][k] == 1) return false;
         }
         return true;
-    }
+    }*/
     //Time : O(n*n*26) ==> O(n*n)
     //Space : O(n*26) ==> O(n)
+    
+    //slightly optimized using hashing method
+        int[] masks = new int[words.length];
+        for(int i = 0;i<words.length;i++){
+            for(char ch : words[i].toCharArray()){
+                masks[i] |= 1 << (ch - 'a');//hashing the words
+            }
+        }
+        int max = 0;
+        //Comparison
+        for(int i = 0;i<words.length;i++){
+            for(int j = i+1;j<words.length;j++){
+                if((masks[i] & masks[j]) == 0) 
+                    max = Math.max(max,words[i].length()*words[j].length());
+            }
+        }
+        return max;
+    }
 }
