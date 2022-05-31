27ce064318e07d0a2ff52fc6f2a5d79e0e7da507
venkateshaPrabhu811 <101506797+venkateshaPrabhu811@users.noreply.github.com>
2022-05-31 20:17:10 +0530

    Time: 19 ms (92.19%), Space: 62.6 MB (58.33%) - LeetHub

--- 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.java
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.java
@@ -13,14 +13,32 @@ class Solution {
         }
         return set.size() == value;*/
         
-        int value = (int)Math.pow(2,k);
+        /*int value = (int)Math.pow(2,k);
         if(s.length() < k) return false;
         HashSet<String> set = new HashSet<>();
         for(int index = 0;index <= s.length()-k;++index){
             set.add(s.substring(index,index+k));
         }
-        return set.size() == value;
+        return set.size() == value;*/
         
         //Time O(n*k)
+        
+        //Optimized
+        //Using roll hash
+        int need = 1<<k;
+        boolean[] done = new boolean[need];
+        int ones = need-1;
+        int currhash = 0;
+        for(int i = 0;i<s.length();++i){
+            currhash = ((currhash << 1) & ones) | s.charAt(i) - '0';
+            if(i >= k-1 && !done[currhash]){
+                done[currhash] = true;
+                --need;
+                if(need == 0) return true;
+            }
+        }
+        return false;
+        //Time : O(n)
+        //Space : O(2*k) all possible strings
     }
 }
