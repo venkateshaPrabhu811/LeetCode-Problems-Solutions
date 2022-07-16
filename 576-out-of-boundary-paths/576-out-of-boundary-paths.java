dc82e6cbbef3e1be25b4150a5e1df7e9ac3e4543
venkateshaPrabhu811 <101506797+venkateshaPrabhu811@users.noreply.github.com>
2022-07-17 00:09:44 +0530

    Time: 18 ms (26.25%), Space: 43 MB (41.67%) - LeetHub

--- 576-out-of-boundary-paths/576-out-of-boundary-paths.java
+++ 576-out-of-boundary-paths/576-out-of-boundary-paths.java
@@ -1,6 +1,6 @@
 class Solution {
     int mod = 1000000007;
-    int[][][] dp;
+    //int[][][] dp;
     public int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
         /*Brute Force
         if(startRow < 0 || startColumn < 0 || startRow >= m || startColumn >= n) return 1;
@@ -13,7 +13,7 @@ class Solution {
         return (a + b + c + d) % mod; */
         
         //Dp
-        dp = new int[m][n][maxMove+1];
+        /*dp = new int[m][n][maxMove+1];
         for(int i = 0;i<m;++i){
             for(int j = 0;j<n;++j){
                 for(int k = 0;k<=maxMove;++k){
@@ -21,9 +21,36 @@ class Solution {
                 }
             }
         }
-        return helper(m,n,maxMove,startRow,startColumn) % mod;
+        return helper(m,n,maxMove,startRow,startColumn) % mod;*/
+        
+        if(maxMove <= 0) return 0;
+        int[][] dir = {{1,0},{-1,0},{0,1},{0,-1}};
+        
+        int result = 0;
+        int[][] count = new int[m][n];
+        count[startRow][startColumn] = 1;
+        
+        for(int step = 0;step < maxMove;++step){
+            int[][] temp = new int[m][n];
+            for(int r = 0;r<m;++r){
+                for(int c = 0;c<n;++c){
+                    for(int[] d : dir){
+                        int nr = r + d[0];
+                        int nc = c + d[1];
+                        if(nr < 0 || nc < 0 || nr >= m || nc >= n){
+                            result = (result + count[r][c]) % mod;
+                        }
+                        else{
+                            temp[nr][nc] = (temp[nr][nc] + count[r][c]) % mod;
+                        }
+                    }
+                }
+            }
+            count = temp;
+        }
+        return result % mod;
     }
-    private int helper(int m, int n, int move, int r, int c) {
+    /*private int helper(int m, int n, int move, int r, int c) {
         if (r < 0 || c < 0 || r >= m || c >= n)
             return 1;
         if (move <= 0)
@@ -32,5 +59,5 @@ class Solution {
             return dp[r][c][move] % mod;
         dp[r][c][move] = ((helper(m, n, move - 1, r + 1, c) % mod + helper(m, n, move - 1, r - 1, c) % mod) % mod + (helper(m, n, move - 1, r, c + 1) % mod + helper(m, n, move - 1, r, c - 1) % mod) % mod ) % mod;
         return dp[r][c][move] % mod;
-    }
+    }*/
 }
