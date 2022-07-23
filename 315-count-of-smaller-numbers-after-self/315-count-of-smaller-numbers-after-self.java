0237e66539e263b40f6ae6b91ae9ca89f4d34881
venkateshaPrabhu811 <101506797+venkateshaPrabhu811@users.noreply.github.com>
2022-07-23 17:30:36 +0530

    Time: 617 ms (24.17%), Space: 59.2 MB (93.25%) - LeetHub

--- 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.java
+++ 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.java
@@ -41,18 +41,49 @@ class Solution {
         for(int i : result) res.add(i);
         return res;*/
         
-        List<Integer> res = new ArrayList<Integer>();     
-
-    count = new int[nums.length];
-    int[] indexes = new int[nums.length];
-    for(int i = 0; i < nums.length; i++){
-    	indexes[i] = i;
+        //MergeSort optimal
+        /*List<Integer> res = new ArrayList<Integer>();     
+        count = new int[nums.length];
+        int[] indexes = new int[nums.length];
+        for(int i = 0; i < nums.length; i++){
+    	    indexes[i] = i;
+        }
+        mergesort(nums, indexes, 0, nums.length - 1);
+        for(int i = 0; i < count.length; i++){
+        	res.add(count[i]);
+        }
+        return res;*/
+        
+        //Binary Search
+        Integer[] ans = new Integer[nums.length];
+        List<Integer> sorted = new ArrayList<>();
+        
+        for(int i = nums.length-1;i>=0;--i){
+            int index = findIndex(sorted,nums[i]);
+            ans[i] = index;
+            sorted.add(index,nums[i]);
+        }
+        return Arrays.asList(ans);
     }
-    mergesort(nums, indexes, 0, nums.length - 1);
-    for(int i = 0; i < count.length; i++){
-    	res.add(count[i]);
-    }
-    return res;
+    private int findIndex(List<Integer> sorted,int target){
+        if(sorted.size() == 0) return 0;
+        int start = 0;
+        int end = sorted.size()-1;
+        
+        if(sorted.get(end) < target) return end+1;
+        if(sorted.get(start) >= target) return 0;
+        
+        while(start + 1 < end){
+            int mid = start + (end - start)/2;
+            if(sorted.get(mid) < target){
+                start = mid + 1;
+            }
+            else{
+                end = mid;
+            }
+        }
+        if(sorted.get(start) >= target) return start;
+        return end;
     }
     
     /*private void mergeSort(OrigNumIndex[] nums,int start,int end,int[] result){
@@ -91,49 +122,51 @@ class Solution {
             nums[start++] = m;
         }
     }*/
-    private void mergesort(int[] nums, int[] indexes, int start, int end){
-	if(end <= start){
-		return;
-	}
-	int mid = (start + end) / 2;
-	mergesort(nums, indexes, start, mid);
-	mergesort(nums, indexes, mid + 1, end);
+    
+    
+    /*private void mergesort(int[] nums, int[] indexes, int start, int end){
+	    if(end <= start){
+		    return;
+	    }
+	    int mid = (start + end) / 2;
+	    mergesort(nums, indexes, start, mid);
+	    mergesort(nums, indexes, mid + 1, end);
 	
-	merge(nums, indexes, start, end);
-}
-private void merge(int[] nums, int[] indexes, int start, int end){
-	int mid = (start + end) / 2;
-	int left_index = start;
-	int right_index = mid+1;
-	int rightcount = 0;    	
-	int[] new_indexes = new int[end - start + 1];
-
-	int sort_index = 0;
-	while(left_index <= mid && right_index <= end){
-		if(nums[indexes[right_index]] < nums[indexes[left_index]]){
-			new_indexes[sort_index] = indexes[right_index];
-			rightcount++;
-			right_index++;
-		}else{
-			new_indexes[sort_index] = indexes[left_index];
-			count[indexes[left_index]] += rightcount;
-			left_index++;
-		}
-		sort_index++;
-	}
-	while(left_index <= mid){
-		new_indexes[sort_index] = indexes[left_index];
-		count[indexes[left_index]] += rightcount;
-		left_index++;
-		sort_index++;
-	}
-	while(right_index <= end){
-		new_indexes[sort_index++] = indexes[right_index++];
-	}
-	for(int i = start; i <= end; i++){
-		indexes[i] = new_indexes[i - start];
-	}
-}
+	    merge(nums, indexes, start, end);
+    }
+    
+    private void merge(int[] nums, int[] indexes, int start, int end){
+	    int mid = (start + end) / 2;
+	    int left_index = start;
+	    int right_index = mid+1;
+	    int rightcount = 0;    	
+	    int[] new_indexes = new int[end - start + 1];\
+	    int sort_index = 0;
+	    while(left_index <= mid && right_index <= end){
+		    if(nums[indexes[right_index]] < nums[indexes[left_index]]){
+			    new_indexes[sort_index] = indexes[right_index];
+			    rightcount++;
+			    right_index++;
+		    }else{
+			    new_indexes[sort_index] = indexes[left_index];
+			    count[indexes[left_index]] += rightcount;
+			    left_index++;
+		    }
+		    sort_index++;
+	    }
+	    while(left_index <= mid){
+		    new_indexes[sort_index] = indexes[left_index];
+		    count[indexes[left_index]] += rightcount;
+		    left_index++;
+		    sort_index++;
+	    }
+	    while(right_index <= end){
+		    new_indexes[sort_index++] = indexes[right_index++];
+	    }
+	    for(int i = start; i <= end; i++){
+		    indexes[i] = new_indexes[i - start];
+    	}
+    }*/
 }
 class OrigNumIndex{
     int val;
