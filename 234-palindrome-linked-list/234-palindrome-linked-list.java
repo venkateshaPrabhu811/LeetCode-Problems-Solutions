e5f918fb67da57443b419a71d05809b1ffa7f0de
venkateshaPrabhu811 <101506797+venkateshaPrabhu811@users.noreply.github.com>
2022-07-25 19:33:32 +0530

    Time: 7 ms (65.58%), Space: 97.6 MB (56.23%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.java
+++ 234-palindrome-linked-list/234-palindrome-linked-list.java
@@ -10,7 +10,7 @@
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        Deque<Integer> q = new ArrayDeque<>();
+        /*Deque<Integer> q = new ArrayDeque<>();
         while(head != null){
             q.add(head.val);
             head = head.next;
@@ -18,6 +18,32 @@ class Solution {
         while(q.size() > 1){
             if(q.pollFirst() != q.pollLast()) return false;
         }
-        return true;
+        return true;*/
+        
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while(fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        if(fast != null) slow = slow.next;
+    
+        slow = reverse(slow);
+        while(slow != null && head.val == slow.val) {
+            head = head.next;
+            slow = slow.next;
+        }
+        return slow == null;
+    }
+    private ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        while(head != null) {
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
     }
 }
