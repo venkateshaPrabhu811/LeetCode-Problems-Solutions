9abef086b8597cf15cdb8ee9e29836714b07559e
venkateshaPrabhu811 <101506797+venkateshaPrabhu811@users.noreply.github.com>
2022-07-28 17:48:43 +0530

    Time: 30 ms (13.69%), Space: 46.6 MB (15.13%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -1,5 +1,6 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
+        /*Sorting without space
         char[] sarr = s.toCharArray();
         char[] tarr = t.toCharArray();
         Arrays.sort(sarr);
@@ -7,6 +8,20 @@ class Solution {
         if(String.valueOf(sarr).equals(String.valueOf(tarr))){
             return true;
         }
-        return false;
+        return false;*/
+        
+        //HashMap Approach
+        HashMap<Character,Integer> map = new HashMap<>();
+        int n = s.length();
+        int m = t.length();
+        if(n != m) return false;
+        for(char ch : s.toCharArray()){
+            map.put(ch,map.getOrDefault(ch,0)+1);
+        }
+        for(char ch : t.toCharArray()){
+            if(!map.containsKey(ch)|| map.get(ch) == 0) return false;
+            map.put(ch,map.get(ch)-1);
+        }
+        return true;
     }
 }
