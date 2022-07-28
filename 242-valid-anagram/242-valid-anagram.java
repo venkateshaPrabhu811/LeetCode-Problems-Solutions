cd75da673adaef09240ea8e78f0a3339b43aef1d
venkateshaPrabhu811 <101506797+venkateshaPrabhu811@users.noreply.github.com>
2022-07-28 18:01:58 +0530

    Time: 6 ms (63.45%), Space: 43.2 MB (77.65%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -10,8 +10,8 @@ class Solution {
         }
         return false;*/
         
-        //HashMap Approach
-        HashMap<Character,Integer> map = new HashMap<>();
+        //HashMap Approach for folow up question
+        /*HashMap<Character,Integer> map = new HashMap<>();
         int n = s.length();
         int m = t.length();
         if(n != m) return false;
@@ -21,6 +21,20 @@ class Solution {
         for(char ch : t.toCharArray()){
             if(!map.containsKey(ch)|| map.get(ch) == 0) return false;
             map.put(ch,map.get(ch)-1);
+        }
+        return true;*/
+        
+        //using array of size 26
+        int[] arr = new int[26];
+        int n = s.length();
+        int m = t.length();
+        if(n != m) return false;
+        for(int i = 0;i<n;++i){
+            ++arr[s.charAt(i) - 'a'];
+            --arr[t.charAt(i) - 'a'];
+        }
+        for(int i = 0;i<26;++i){
+            if(arr[i] != 0) return false;
         }
         return true;
     }
