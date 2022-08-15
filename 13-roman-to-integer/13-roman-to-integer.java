116aa5e74cf65ad4cea9282f9fdf3dfe8c3ce690
venkateshaPrabhu811 <101506797+venkateshaPrabhu811@users.noreply.github.com>
2022-08-15 19:34:45 +0530

    Time: 5 ms (94.28%), Space: 45.3 MB (66.40%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.java
+++ 13-roman-to-integer/13-roman-to-integer.java
@@ -1,6 +1,6 @@
 class Solution {
     public int romanToInt(String s) {
-        HashMap<Character, Integer> romToInt = new HashMap<Character, Integer>();
+        /*HashMap<Character, Integer> romToInt = new HashMap<Character, Integer>();
         romToInt.put('I', 1);
         romToInt.put('V', 5);
         romToInt.put('X', 10);
@@ -19,6 +19,23 @@ class Solution {
                 test = val;
             }
         }
-        return sum;
+        return sum;*/
+        
+        int num = 0;
+        int ans = 0;
+        for(int i = s.length()-1;i>=0;--i){
+            switch(s.charAt(i)){
+                case 'I' : num = 1; break;
+                case 'V' : num = 5; break;
+                case 'X' : num = 10; break;
+                case 'L' : num = 50; break;
+                case 'C' : num = 100; break;
+                case 'D' : num = 500; break;
+                case 'M' : num = 1000; break;
+            }
+            if(4 * num < ans) ans -= num;
+            else ans += num;
+        }
+        return ans;
     }
 }
