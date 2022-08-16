bb46663cd278137e2ff2f3d2ddcb8ea09196461a
venkateshaPrabhu811 <101506797+venkateshaPrabhu811@users.noreply.github.com>
2022-08-16 09:56:20 +0530

    Time: 11 ms (80.68%), Space: 48.3 MB (77.52%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.java
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.java
@@ -20,7 +20,7 @@ class Solution {
         }
         return q.peek();*/
         
-        HashMap<Character,Integer> map = new HashMap<>();
+        /*HashMap<Character,Integer> map = new HashMap<>();
         for(int i = 0;i<s.length();i++){
             char ch = s.charAt(i);
             map.put(ch,map.getOrDefault(ch,0)+1);
@@ -30,6 +30,36 @@ class Solution {
                 return i;
             }
         }
-        return -1;
+        return -1;*/
+        
+        /*int[] freq = new int[26];
+        int index = -1;
+        for(int i = s.length()-1;i>=0;--i){
+            char ch = s.charAt(i);
+            if(freq[ch - 'a'] == 0){
+                index = i;
+            }
+            freq[ch - 'a']++;
+        }
+        return (index >= 0 && freq[s.charAt(index) - 'a'] == 1) ? index : -1;*/
+        int[] freq = new int[26];
+        Arrays.fill(freq,-1);
+        for(int i = 0;i<s.length();++i){
+            int ch = s.charAt(i) - 'a';
+            if(freq[ch] == -1){
+                freq[ch] = i;
+            }
+            else if(freq[ch] >= 0){
+                freq[ch] = -2;
+            }
+        }
+        int min = -1;
+        for(int i = 0;i<26;++i){
+            if(freq[i] >= 0){
+                if(min == -1) min = freq[i];
+                else min = freq[i] < min ? freq[i] : min;
+            }
+        }
+        return min;
     }
 }
