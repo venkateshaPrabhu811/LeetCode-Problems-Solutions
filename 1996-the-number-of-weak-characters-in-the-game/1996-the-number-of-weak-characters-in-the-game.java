501c2f1f5bbb6a0a1f6baa2c6ec5a2f997646bff
venkateshaPrabhu811 <101506797+venkateshaPrabhu811@users.noreply.github.com>
2022-09-09 19:25:43 +0530

    Time: 30 ms (88.26%), Space: 138 MB (22.43%) - LeetHub

--- 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.java
+++ 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.java
@@ -1,11 +1,24 @@
 class Solution {
     public int numberOfWeakCharacters(int[][] properties) {
-        Arrays.sort(properties, (a,b) -> (a[0] == b[0]) ? b[1] - a[1] : a[0] - b[0]);
+        /*Arrays.sort(properties, (a,b) -> (a[0] == b[0]) ? b[1] - a[1] : a[0] - b[0]);
         int count = 0;
         int max = Integer.MIN_VALUE;
         for(int i = properties.length-1;i>=0;--i){
             if(properties[i][1] < max) ++count;
             max = Math.max(max,properties[i][1]);
+        }
+        return count;*/
+        
+        int count = 0;
+        int[] dp = new int[100002];
+        for(int[] arr : properties){
+            dp[arr[0]] = Math.max(dp[arr[0]],arr[1]);
+        }
+        for(int i = 100000;i>0;--i){
+            dp[i-1] = Math.max(dp[i],dp[i-1]);
+        }
+        for(int[] arr : properties){
+            count += dp[arr[0] + 1] > arr[1] ? 1 : 0;
         }
         return count;
     }
